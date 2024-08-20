cfa757d32372fe850fa5a0df07977b7b572b987b
JNguyennP <jnguyenpham83@gmail.com>
2024-08-20 14:45:25 +0100

    Updated routers file

--- routes/fruits.js
+++ routes/fruits.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const fruits = require ('../controllers/fruits')
+const fruits = require ('../controllers/fruits');
 
-router.get('/', fruits.index)
+router.get('/', fruits.index);
 
 router.get('/:name', fruits.show);
+
+router.post('/', fruits.create);
+
+router.patch('/:name', fruits.update);
+
+router.delete('/:name', fruits.destroy);
 
 module.exports = router
